16bb32a5fa5cfbba47c5f3faeb12b74b2d2021ca
redacted24 <huangq4@outlook.com>
2024-01-27 14:11:49 -0500

    implemented webcam, modified readme to include React Webcam as a dependency

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,21 @@
+import './index.css'
+import Webcam from 'react-webcam'
+
 const App = () => {
-  return (
-    <div>
-      hi
-    </div>
-  )
+
+	// Some Rules for the Webcam. Entire object is passed as a prop in the Webcam component
+	const videoConstraints = {
+		width: { min: 1080},
+		height: { min: 720},
+		aspectRatio: 0.666666666666666667
+	}
+	return (
+		<div>
+			<div id='webcam-container'>
+				<Webcam mirrored={true} imageSmoothing={true} audio={false} videoConstraints={videoConstraints}/>	
+			</div>
+		</div>
+	)
 }
 
 export default App
