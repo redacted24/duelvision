37eb483b6ac2c0b4b0b6287a4ee401280bda5bc4
Crazydodo <peizheg@gmail.com>
2024-01-27 16:02:00 -0500

    fixed websocket client

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,17 +1,33 @@
 import './index.css'
 import Webcam from 'react-webcam'
 
-let url = 'ws://192.168.173.38:7890'
-
-let socket = new WebSocket(url)
-
-socket.send('hi')
-
-socket.onmessage = async (event) => {
-  console.log(event)
-}
-
 const App = () => {
+
+  let url = 'ws://192.168.173.38:8001'
+
+  let socket = new WebSocket(url);
+  
+  const clicky = () => {
+    const obj = { hello: "world" };
+    socket.send(new Blob([JSON.stringify(obj, null, 2)], {
+      type: "application/json",
+    }));
+    return false;
+  };
+  
+  socket.onmessage = async (event) => {
+    let incomingMessage = event.data;
+    console.log(incomingMessage)
+  };
+  
+  socket.onclose = event => console.log(`Closed ${event.code}`)
+  
+  function showMessage(message) {
+    let messageElem = document.createElement('div');
+    messageElem.textContent = message;
+    document.getElementById('messages').prepend(messageElem);
+  }
+
 
 	// Some Rules for the Webcam. Entire object is passed as a prop in the Webcam component
 	const videoConstraints = {
@@ -24,6 +40,7 @@ const App = () => {
 			<div id='webcam-container'>
 				<Webcam mirrored={true} imageSmoothing={true} audio={false} videoConstraints={videoConstraints}/>	
 			</div>
+      <button onClick={clicky}>click</button>
 		</div>
 	)
 }
