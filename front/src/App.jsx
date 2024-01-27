a3abf20c945a372b8e39133823099b6334953230
Crazydodo <peizheg@gmail.com>
2024-01-27 15:27:04 -0500

    websocket client

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,16 @@
 import './index.css'
 import Webcam from 'react-webcam'
 
+let url = 'ws://192.168.173.38:7890'
+
+let socket = new WebSocket(url)
+
+socket.send('hi')
+
+socket.onmessage = async (event) => {
+  console.log(event)
+}
+
 const App = () => {
 
 	// Some Rules for the Webcam. Entire object is passed as a prop in the Webcam component
