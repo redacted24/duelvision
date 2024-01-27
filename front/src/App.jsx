a42fc892a4dd32e136df85381c94cc457d87deaf
Crazydodo <peizheg@gmail.com>
2024-01-27 16:43:17 -0500

    add camera capture function

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,46 +1,27 @@
 import './index.css'
-import Webcam from 'react-webcam'
+import Camera from './Camera'
 
 const App = () => {
-
   let url = 'ws://192.168.173.38:8001'
-
   let socket = new WebSocket(url);
-  
-  const clicky = () => {
-    const obj = { hello: "world" };
-    socket.send(new Blob([JSON.stringify(obj, null, 2)], {
-      type: "application/json",
-    }));
+
+  const sendMessage = (message) => {
+    socket.send(message);
     return false;
   };
-  
+
   socket.onmessage = async (event) => {
     let incomingMessage = event.data;
     console.log(incomingMessage)
   };
-  
+
   socket.onclose = event => console.log(`Closed ${event.code}`)
-  
-  function showMessage(message) {
-    let messageElem = document.createElement('div');
-    messageElem.textContent = message;
-    document.getElementById('messages').prepend(messageElem);
-  }
 
-
-	// Some Rules for the Webcam. Entire object is passed as a prop in the Webcam component
-	const videoConstraints = {
-		width: { min: 1080},
-		height: { min: 720},
-		aspectRatio: 0.666666666666666667
-	}
 	return (
 		<div>
-			<div id='webcam-container'>
-				<Webcam mirrored={true} imageSmoothing={true} audio={false} videoConstraints={videoConstraints}/>	
+			<div>
+        <Camera sendMessage={sendMessage} />
 			</div>
-      <button onClick={clicky}>click</button>
 		</div>
 	)
 }
---
+++ front/src/Camera.jsx
@@ -0,0 +1,41 @@
+import React from "react"
+import Webcam from 'react-webcam'
+import { useEffect } from "react";
+
+const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
+  };
+  
+const Camera = ({ sendMessage }) => {
+    const webcamRef = React.useRef(null)
+
+    const capture = React.useCallback(
+        () => {
+            const imageSrc = webcamRef.current.getScreenshot();
+            sendMessage(imageSrc)
+        },
+        [webcamRef]
+    )
+
+    useEffect(() => {
+        const interval = setInterval(() => capture(), 1000 / 30);
+
+        return () => clearInterval(interval)
+    }, [])
+
+
+    return (
+        <Webcam
+            audio={false}
+            height={720}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={1280}
+            videoConstraints={videoConstraints}
+        />
+    )
+}
+
+export default Camera
