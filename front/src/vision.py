0574e23adfcf215e9910f3c8942b422b2277c112
CFung721 <cfung05@gmail.com>
2024-01-27 17:24:01 -0500

    vision2

--- front/src/vision.py
+++ front/src/vision.py
@@ -1,30 +1,32 @@
 import mediapipe as mp
 import cv2 as cv
+import base64
 import numpy as np
-
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
-
+#asdf = "------------------"
 def image(file):
-    cam = cv.flip(cv.imread(file), 1)
-    with mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.5) as hands:
-        frame = cam
-        
-        image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        image.flags.writeable = False
-        
-        results = hands.process(image)
-        
-        image.flags.writeable = True
-        image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
-        if results.multi_hand_landmarks:
-            for landmark in results.multi_hand_landmarks:
-                print(landmark)
-                mp_drawing.draw_landmarks(image, landmark, mp_hands.HAND_CONNECTIONS)
-        
-        cv.imshow('Image', cv.flip(image, 1))
-        cv.waitKey(0)
+    with open(file, "rb") as base64_file:
+        arr = np.frombuffer(base64.b64decode(base64_file.read()), np.uint8)
+        frame = cv.flip(cv.imdecode(arr, cv.IMREAD_COLOR), 1)
+        with mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.5) as hands:
+            #print(asdf, frame)
+            image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            image.flags.writeable = False
+            
+            #print(asdf, image)
+            results = hands.process(image)
+            
+            image.flags.writeable = True
+            image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+            if results.multi_hand_landmarks:
+                for landmark in results.multi_hand_landmarks:
+                    print(landmark)
+                    mp_drawing.draw_landmarks(image, landmark, mp_hands.HAND_CONNECTIONS)
+            
+            cv.imshow('Image', cv.flip(image, 1))
+            cv.waitKey(0)
 
 def video():
     cam = cv.VideoCapture(0)
