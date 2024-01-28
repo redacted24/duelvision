5cd30f2c32a39b3ed246bfaa3f6aa0e25a199102
Crazydodo <peizheg@gmail.com>
2024-01-28 09:19:09 -0500

    add websocket

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,23 +7,20 @@ import Leaderboard from './components/Leaderboard'
 import GamePage from './components/GamePage'
 import Lobby from './components/Lobby'
 import Statistics from './components/Statistics'
-import { HandLandmarker, FilesetResolver } from '@mediapipe/tasks-vision'
 
 const App = () => {
-  const [username, setUsername] = useState(null)
-  
+    const [username, setUsername] = useState(null)
 
 	return (
-	<Router>
-		<Header />
-		<Routes>
-			<Route path = "/" element = {(!username) ? (<Login setUsername={setUsername}/>) : (<Lobby/>)}/>
-			<Route path = "/gamepage" element = {<GamePage/>}/>
-			<Route path = "/leaderboard" element = {<Leaderboard />}/>
-			<Route path = "/statistics" element = {<Statistics />}/>
-		</Routes>
-	</Router>
-
-	)}	
+        <Router>
+            <Header />
+            <Routes>
+                <Route path = "/" element = {(!username) ? (<Login setUsername={setUsername}/>) : (<Lobby/>)}/>
+                <Route path = "/gamepage" element = {<GamePage/>}/>
+                <Route path = "/leaderboard" element = {<Leaderboard />}/>
+                <Route path = "/statistics" element = {<Statistics />}/>
+            </Routes>
+        </Router>
+)}	
 	
 export default App
