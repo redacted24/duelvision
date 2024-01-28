b6a94e91def9a99938fcca5756e8d96e14024602
redacted24 <huangq4@outlook.com>
2024-01-27 23:08:52 -0500

    added styling for the lobby button

--- front/src/components/Lobby.jsx
+++ front/src/components/Lobby.jsx
@@ -1,7 +1,12 @@
+import '../styles/lobby.css'
+import { Link } from 'react-router-dom'
+
 const Lobby = () => {
     return (
-        <div>
-            Lobby
+        <div id="main-container">
+            <div className='options-container' id='multiplayer'>Multiplayer</div>
+            <div className='options-container' id='statistics'>Statistics</div>
+            <div className='options-container' id='leaderboard'>Leaderboard</div>
         </div>
     )
 }
