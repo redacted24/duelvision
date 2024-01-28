0207d7baba09c818ad1fab8be0d08ee36267c0f1
redacted24 <huangq4@outlook.com>
2024-01-28 10:45:13 -0500

    added link from leaderboards to statistics

--- front/src/components/Leaderboard.jsx
+++ front/src/components/Leaderboard.jsx
@@ -1,5 +1,6 @@
 import '../styles/leaderboard.css'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Leaderboard = ({ username }) => {
 
@@ -10,15 +11,17 @@ const Leaderboard = ({ username }) => {
     return (
         <div>
             <div id="page-title">Leaderboard</div>
-            <div className='lead-container'>
-                <div className='leaderNumber greenColor'>1</div>
-                <div className='leaderName greenColor'>{username}</div>
-                <div className='leaderStats greenColor'>
-                    <div>Wins {win}</div>
-                    <div>Losses: {loss}</div>
-                    <div>Win-loss ratio: {lossRatio}</div>
+            <Link id="linkers" to='/statistics'>
+                <div className='lead-container'>
+                    <div className='leaderNumber greenColor'>1</div>
+                    <div className='leaderName greenColor'>{username}</div>
+                    <div className='leaderStats greenColor'>
+                        <div>Wins {win}</div>
+                        <div>Losses: {loss}</div>
+                        <div>Win-loss ratio: {lossRatio}</div>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
     )
 }
