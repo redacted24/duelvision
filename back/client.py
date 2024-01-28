dc69bd6bac83c1b70077c96ec0e988f3870686d3
Siva <siva_gaius@email.com>
2024-01-28 03:34:10 -0500

    server complete with client

---
+++ back/client.py
@@ -0,0 +1,38 @@
+#! /bin/python
+
+import asyncio
+import websockets
+import time
+import json
+
+
+async def sender():
+    url = "ws://127.0.0.1:8001"
+    connection = False
+    async with websockets.connect(url) as w:
+        while True:
+            msg = await w.recv()
+            print(msg)
+            if not connection:
+                if msg == "hello":
+                    await w.send("hello")
+                    connection = True
+                    continue
+
+            if msg == "hello":
+                print("hello again")
+                continue
+            blob_dict = json.loads(msg)
+
+            if blob_dict["status_wait"] == True:
+                continue
+
+            blob_dict["x"] = "bob"
+            await w.send(json.dumps(blob_dict))
+
+            
+
+
+            print(msg)
+
+asyncio.run(sender())
--- back/server.py
+++ back/server.py
@@ -1,27 +1,104 @@
 #! /bin/python
 import asyncio
 import websockets
-from vision import image
 import json
 import time
 
-async def handler(websocket):
-    count = 0
-    while True:
-        message = await websocket.recv()
-        try:
-            b64_img = message.split(',')[1]
-            #print(message)
-            #print("\n----\n")
-            coord_ls = image(b64_img)
-            #a = time.time()
-            await websocket.send(json.dumps({ 'coordinates': coord_ls }))
-            #b = time.time()
-            #print(1/(b - a))
-            #print(image(b64_img))
-        except:
-            print("error")
+"""
+INITIATION
+client connect
+recv: json
+{
+player color
+bullet color
+}
+send echo hello
+respond with hello
+success
 
+ESTABLISHED
+if 2nd player not connected send wait signal
+when second player connected (if the list is 1 before appending, send signal to waiting one that it is ready to play)
+
+GAME START
+when hit the edge, send data json
+{
+x: coord
+y: coord
+}
+
+check the socket
+send it to the opposing one
+
+when receiving opposing
+do the same
+game loop
+
+Todo
+handle connections drops
+if kill is sent from client or pinging client periodically does not work, then pause game and await 
+
+"""
+
+status_wait = { "x": 0, "status_wait": True }
+status_ready = { "x": 0, "status_wait": False }
+
+
+clients = []
+
+async def initiate(websocket):
+    global clients
+    if len(clients) <= 2 and websocket not in clients:
+        # server hello
+        await websocket.send("hello")
+
+        response = await websocket.recv()
+
+        if response == "hello":
+            clients.append(websocket)
+
+            if len(clients) == 2:
+                await clients[0].send(json.dumps(status_ready))
+                await clients[1].send(json.dumps(status_ready))
+
+            else:
+                await websocket.send(json.dumps(status_wait))
+
+async def handler(websocket):
+    global clients
+
+    if len(clients) >= 2:
+        return 0
+
+    # initiation
+    if websocket not in clients:
+        await initiate(websocket)
+
+
+    #client_id = clients.index(websocket)
+    while True:
+        try:
+            msg = await websocket.recv()
+
+            client_idx = clients.index(websocket)
+            
+            await clients[client_idx - 1].send(msg)
+
+
+
+
+            #print(dir(websocket))
+            #await websocket.send(f"[{time.time()}] ID: {client_id} You said: {msg}")
+
+        except websockets.exceptions.ConnectionClosed as e:
+            print(e)
+            client_idx = clients.index(websocket)
+            await clients[client_idx - 1].send(json.dumps(status_wait))
+            clients.remove(websocket)
+            break
+
+        #except Exception as e:
+        #    print("WEBSOCKET ERROR:", e)
 
 
 async def main():
