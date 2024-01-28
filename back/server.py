6a65b3b5d8ace0d522b721376ac804cb59a27804
Siva <siva_gaius@email.com>
2024-01-27 20:45:19 -0500

    server test on cyrus laptop

--- back/server.py
+++ back/server.py
@@ -1,16 +1,27 @@
 #! /bin/python
 import asyncio
 import websockets
-
+from vision import image
+import json
+import time
 
 async def handler(websocket):
     count = 0
     while True:
         message = await websocket.recv()
-        print(message)
-        print("\n----\n")
+        try:
+            b64_img = message.split(',')[1]
+            #print(message)
+            #print("\n----\n")
+            coord_ls = image(b64_img)
+            #a = time.time()
+            await websocket.send(json.dumps({ 'coordinates': coord_ls }))
+            #b = time.time()
+            #print(1/(b - a))
+            #print(image(b64_img))
+        except:
+            print("error")
 
-        await websocket.send("sexy man")
 
 
 async def main():
--- back/vision.py
+++ back/vision.py
@@ -2,18 +2,28 @@ import mediapipe as mp
 import cv2 as cv
 import base64
 import numpy as np
+import time
+
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 def image(b64_string):
+    a = time.time()
     arr = np.frombuffer(base64.b64decode(b64_string), np.uint8)
     frame = cv.flip(cv.imdecode(arr, cv.IMREAD_COLOR), 1)
+    b = time.time()
+    print("base64 process",1/(b-a))
     with mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.5) as hands:
         image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
         image.flags.writeable = False
         
+        a = time.time()
         results = hands.process(image)
         
+        b = time.time()
+        print("process hands",1/(b-a))
+
+        a = time.time()
         image.flags.writeable = True
         image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
         all_landmarks = []
@@ -22,6 +32,8 @@ def image(b64_string):
                 mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS)
                 for landmark in hand.landmark:
                     all_landmarks.append([landmark.x, landmark.y])
+        b = time.time()
+        print("getting landmarks",1/(b-a))
         #cv.imshow('Image', cv.flip(image, 1))
         #cv.waitKey(0)
         return(all_landmarks)
